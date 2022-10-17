442fbfa486a25da8e29663b6caf595aee2f753b9
Vanshika001 <vanshikaraman3@gmail.com>
2022-10-18 01:01:25 +0530

    second commit hash

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
@@ -16,8 +17,8 @@ const UserSchema = new mongoose.Schema({
     password:String
 });
 
-const secret = process.env.SECRET;
-mongoose.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+// const secret = process.env.SECRET;
+// mongoose.plugin(encrypt,{secret:secret,encryptedFields:['password']});
 
 app.use(express.static("public"));
 app.set("view engine",'ejs');
@@ -39,7 +40,7 @@ app.get("/login",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -52,7 +53,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){
         if(err){
